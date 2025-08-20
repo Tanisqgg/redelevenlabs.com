445403c61de41035820f105e480b891eb1dc1221
Tanisqgg <tanisqbpm@gmail.com>
2025-08-19 21:11:12 -0500

    location update

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,5 @@
-import { Navbar } from "@/components/Navbar";
-import { Hero } from "@/components/Hero";
-import { Features } from "@/components/Features";
-import { HowItWorks } from "@/components/HowItWorks";
-import { FAQ } from "@/components/FAQ";
-import { CTA } from "@/components/CTA";
-import { Footer } from "@/components/Footer";
-import Contact from "./sections/Contact";
-import { ComingSoon } from "@/components/ComingSoon";
+import { redirect } from 'next/navigation';
 
 export default function Page() {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Features />
-      <HowItWorks />
-      <ComingSoon />
-      <FAQ />
-      <Contact />
-      <CTA />
-      <Footer />
-    </main>
-  );
+  redirect('/upgrades');
 }
