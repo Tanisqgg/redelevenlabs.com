b41d76f6d3abcad3dbe3c4c73916859e564ea87a
Tanisq Jawahar <84832957+Tanisqgg@users.noreply.github.com>
2026-06-20 17:12:35 -0500

    Configure site for root custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
-const repoName = "r11_website";
-const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
@@ -8,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: isGitHubPages ? `/${repoName}` : "",
-  assetPrefix: isGitHubPages ? `/${repoName}/` : "",
   experimental: {
     typedRoutes: true,
   },
